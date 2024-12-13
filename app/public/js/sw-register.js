6a47433afcd76abe785cce5870cf83c9d7d5688e
Ana Jasso <ana.jassov@gmail.com>
2024-12-13 12:28:27 -0600

    PWA prueba 2

--- app/public/js/sw-register.js
+++ app/public/js/sw-register.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js').then((registration) => {
+      navigator.serviceWorker.register('/public/js/sw.js').then((registration) => {
         console.log('Service Worker registrado con éxito:', registration);
       }).catch((error) => {
         console.log('Error al registrar el Service Worker:', error);
