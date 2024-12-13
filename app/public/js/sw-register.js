38874b8cd10201ae733b8be6981eba61abf766ea
Ana Jasso <ana.jassov@gmail.com>
2024-12-13 12:33:20 -0600

    PWA rutas correctas

--- app/public/js/sw-register.js
+++ app/public/js/sw-register.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/public/js/sw.js').then((registration) => {
+      navigator.serviceWorker.register('/app/sw.js').then((registration) => {
         console.log('Service Worker registrado con éxito:', registration);
       }).catch((error) => {
         console.log('Error al registrar el Service Worker:', error);
